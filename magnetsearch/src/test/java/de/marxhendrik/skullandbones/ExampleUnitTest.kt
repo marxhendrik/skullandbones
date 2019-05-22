d8b2f672f6535b7b9b0493ed0b96e573165545e4
Hendrik Marx <h.marx@mytaxi.com>
2019-05-22 19:59:07 +0200

    setup a test file as playground

---
+++ magnetsearch/src/test/java/de/marxhendrik/skullandbones/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package de.marxhendrik.skullandbones
+
+import org.junit.Ignore
+
+/**
+ * Do actual requests against api, so not meant as a real test, just for exploring the APIs
+ */
+@Ignore("should not run except when explicitly executed")
+class ApiIntegrationTest {
+
+
+}
