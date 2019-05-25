98318c87e6f57aa49c8feb913e382049a489ce2a
Hendrik Marx <h.marx@mytaxi.com>
2019-05-25 22:14:11 +0200

    move coroutine related thing back up into use case

--- magnetsearch/src/main/java/de/marxhendrik/skullandbones/magnetsearch/data/repo/MagnetSearchRepo.kt
+++ magnetsearch/src/main/java/de/marxhendrik/skullandbones/magnetsearch/data/repo/MagnetSearchRepo.kt
@@ -3,13 +3,8 @@ package de.marxhendrik.skullandbones.magnetsearch.data.repo
 import de.marxhendrik.skullandbones.magnetsearch.data.api.MagnetSearchApi
 import de.marxhendrik.skullandbones.magnetsearch.data.model.SearchResult
 import javax.inject.Inject
-import kotlin.coroutines.suspendCoroutine
 
 class MagnetSearchRepo @Inject constructor(private val searchApi: MagnetSearchApi) {
 
-    suspend fun search(query: String): List<SearchResult> {
-        return suspendCoroutine { continuation ->
-            continuation.resumeWith(Result.success(searchApi.search(query)))
-        }
-    }
+    fun search(query: String): List<SearchResult> = searchApi.search(query)
 }
