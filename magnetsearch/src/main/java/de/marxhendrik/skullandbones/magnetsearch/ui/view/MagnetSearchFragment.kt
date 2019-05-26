06dde81b6e3edbd4baf0088ed997586d9b1ee2b9
Hendrik Marx <h.marx@mytaxi.com>
2019-05-26 20:34:33 +0200

    rename layout: remove "fragment" prefix

--- magnetsearch/src/main/java/de/marxhendrik/skullandbones/magnetsearch/ui/view/MagnetSearchFragment.kt
+++ magnetsearch/src/main/java/de/marxhendrik/skullandbones/magnetsearch/ui/view/MagnetSearchFragment.kt
@@ -2,22 +2,27 @@ package de.marxhendrik.skullandbones.magnetsearch.ui.view
 
 import de.marxhendrik.skullandbones.core.base.fragment.BaseDataBindingFragment
 import de.marxhendrik.skullandbones.magnetsearch.R
-import de.marxhendrik.skullandbones.magnetsearch.databinding.FragmentMagnetSearchBinding
+import de.marxhendrik.skullandbones.magnetsearch.databinding.MagnetSearchBinding
 import de.marxhendrik.skullandbones.magnetsearch.di.buildAndInject
 import de.marxhendrik.skullandbones.magnetsearch.ui.presentation.MagnetSearchUiController
+import kotlinx.android.synthetic.main.magnet_search.*
 import javax.inject.Inject
 
 class MagnetSearchFragment(
-    override val layoutId: Int = R.layout.fragment_magnet_search
-) : BaseDataBindingFragment<FragmentMagnetSearchBinding>() {
+    override val layoutId: Int = R.layout.magnet_search
+) : BaseDataBindingFragment<MagnetSearchBinding>() {
 
     @Inject
     lateinit var uiController: MagnetSearchUiController
 
-    override fun bind(binding: FragmentMagnetSearchBinding) {
+    override fun bind(binding: MagnetSearchBinding) {
         binding.data = uiController
     }
 
     override fun inject() = buildAndInject()
 
+    override fun onViewCreated() {
+        searchView.setIconifiedByDefault(false)
+        searchView.isSubmitButtonEnabled = false
+    }
 }
