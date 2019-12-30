07f01f6cb91da49e6e012023a7c6a3d3306d5a13
Hendrik Marx <h.marx@mytaxi.com>
2019-12-30 21:17:51 +0100

    Add TODO

--- app/src/main/java/de/marxhendrik/skullandbones/ui/RootActivity.kt
+++ app/src/main/java/de/marxhendrik/skullandbones/ui/RootActivity.kt
@@ -8,8 +8,6 @@ import de.marxhendrik.skullandbones.R
 import de.marxhendrik.skullandbones.core.navigation.NavigationActivity
 import de.marxhendrik.skullandbones.rib.search.builder.Search
 import de.marxhendrik.skullandbones.rib.search.builder.SearchBuilder
-import de.marxhendrik.skullandbones.root.navigation.Feature
-import de.marxhendrik.skullandbones.root.navigation.addFeature
 import kotlinx.android.synthetic.main.activity_root.*
 
 class RootActivity : RibActivity(), NavigationActivity {
@@ -17,14 +15,18 @@ class RootActivity : RibActivity(), NavigationActivity {
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_root)
         super.onCreate(savedInstanceState)
-        addFeature(Feature.MagnetSearch)
     }
 
     override val rootViewGroup: ViewGroup
         get() = rootId
 
     override fun createRib(savedInstanceState: Bundle?): Node<*> {
+
+        //FIXME REMOVE WHEN 'SEARCH' IS FINISHED
+//        addFeature(Feature.MagnetSearch)
+
         return SearchBuilder(
+            // TODO provide dependencies from a Module
             object : Search.Dependency {}
 
         ).build(savedInstanceState)
