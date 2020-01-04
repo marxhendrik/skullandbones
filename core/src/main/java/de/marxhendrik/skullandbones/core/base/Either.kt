5ad2f9c6d07b41c5652ed28e5644e50ee748fa92
Hendrik Marx <h.marx@mytaxi.com>
2020-01-04 20:03:46 +0100

    Remove left/right method from Either class and rename a/b to value

--- core/src/main/java/de/marxhendrik/skullandbones/core/base/Either.kt
+++ core/src/main/java/de/marxhendrik/skullandbones/core/base/Either.kt
@@ -1,26 +1,23 @@
 package de.marxhendrik.skullandbones.core.base
 
 sealed class Either<out L, out R> {
-    data class Left<out L>(val a: L) : Either<L, Nothing>()
+    data class Left<out L>(val value: L) : Either<L, Nothing>()
 
-    data class Right<out R>(val b: R) : Either<Nothing, R>()
+    data class Right<out R>(val value: R) : Either<Nothing, R>()
 
     val isRight get() = this is Right<R>
     val isLeft get() = this is Left<L>
 
-    fun <L> left(a: L) = Left(a)
-    fun <R> right(b: R) = Right(b)
-
     fun on(failure: (L) -> Unit, success: (R) -> Unit) {
         when (this) {
-            is Left -> failure(a)
-            is Right -> success(b)
+            is Left -> failure(value)
+            is Right -> success(value)
         }
     }
 }
 
 fun <T, L, R> Either<L, R>.flatMap(fn: (R) -> Either<L, T>): Either<L, T> =
     when (this) {
-        is Either.Left -> Either.Left(a)
-        is Either.Right -> fn(b)
+        is Either.Left -> Either.Left(value)
+        is Either.Right -> fn(value)
     }
